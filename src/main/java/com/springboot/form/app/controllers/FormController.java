b56d142fc89b0e743bed4fe591c5c7d6f6fdf565
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-01 20:51:24 -0500

    Redirect despues del POST procesar

--- src/main/java/com/springboot/form/app/controllers/FormController.java
+++ src/main/java/com/springboot/form/app/controllers/FormController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.bind.support.SessionStatus;
 
@@ -36,82 +37,81 @@ import com.springboot.form.app.validation.UsuarioValidador;
 @Controller
 @SessionAttributes("usuario")
 public class FormController {
-	
+
 	@Autowired
 	private UsuarioValidador validador;
-	
+
 	@Autowired
 	private PaisService paisService;
-	
+
 	@Autowired
 	private RoleService roleService;
-	
+
 	@Autowired
 	private PaisPropertyEditor paisEditor;
-	
+
 	@Autowired
 	private RolesEditor rolEditor;
-	
+
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.addValidators(validador);
 		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 		dateFormat.setLenient(false);
 		binder.registerCustomEditor(Date.class, "fechaNacimiento", new CustomDateEditor(dateFormat, false));
-		
+
 		binder.registerCustomEditor(String.class, "nombre", new NombreMayusculaEditor());
 		binder.registerCustomEditor(String.class, "apellido", new NombreMayusculaEditor());
-		
+
 		binder.registerCustomEditor(Pais.class, "pais", paisEditor);
 		binder.registerCustomEditor(Rol.class, "roles", rolEditor);
 	}
-	
+
 	@ModelAttribute("genero")
-	public List<String> genero(){
+	public List<String> genero() {
 		return Arrays.asList("Hombre", "Mujer");
 	}
-	
+
 	@ModelAttribute("listaRoles")
-	public List<Rol> listaRoles(){
+	public List<Rol> listaRoles() {
 		return this.roleService.listar();
 	}
-	
+
 	@ModelAttribute("listaPaises")
-	public List<String> paises(){
+	public List<String> paises() {
 		return Arrays.asList("España", "Argentina", "Colombia", "Mexico", "Chile", "Peru", "Venezuela");
 	}
-	
+
 	@ModelAttribute("listaPaises")
-	public List<Pais> listaPaises(){
+	public List<Pais> listaPaises() {
 		return paisService.listar();
 	}
-	
+
 	@ModelAttribute("listaRolesString")
-	public List<String> listaRolesString(){
+	public List<String> listaRolesString() {
 		List<String> roles = new ArrayList<>();
 		roles.add("ROLE_ADMIN");
 		roles.add("ROLE_USER");
 		roles.add("ROLE_MODERATOR");
-		
+
 		return roles;
 	}
-	
+
 	@ModelAttribute("listaRolesMap")
-	public Map<String, String> listaRolesMap(){
+	public Map<String, String> listaRolesMap() {
 		Map<String, String> roles = new HashMap<String, String>();
-		
+
 		roles.put("ROLE_ADMIN", "Administridor");
 		roles.put("ROLE_USER", "Usuario");
 		roles.put("ROLE_MODERATOR", "Moderador ");
-		
-		
+
 		return roles;
 	}
-	
+
 	@ModelAttribute("paisesMap")
-	public Map<String, String> paisesMap(){
+	public Map<String, String> paisesMap() {
 		Map<String, String> paises = new HashMap<String, String>();
-		
+
 		paises.put("ES", "España");
 		paises.put("MX", "Mexico");
 		paises.put("CO", "Colombia");
@@ -119,7 +119,7 @@ public class FormController {
 		paises.put("AR", "Argentina");
 		paises.put("PE", "Peru");
 		paises.put("VE", "Venezuela");
-		
+
 		return paises;
 	}
 
@@ -137,26 +137,35 @@ public class FormController {
 		model.addAttribute("usuario", usuario);
 		return "form";
 	}
-	
-	@PostMapping("/form")
-	public String procesar(@Valid Usuario usuario, BindingResult result, Model model, SessionStatus status) {
 
-		//validador.validate(usuario, result);
-		model.addAttribute("titulo", "Resultado form");
+	@PostMapping("/form")
+	public String procesar(@Valid Usuario usuario, BindingResult result, Model model) {
+
+		// validador.validate(usuario, result);
 		
-		if(result.hasErrors()) {
-			/*Map<String, String> errores = new HashMap<>();
-			result.getFieldErrors().forEach(err ->{
-				errores.put(err.getField(), "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()));
-			});
-			model.addAttribute("error", errores);
-			*/
+
+		if (result.hasErrors()) {
+			model.addAttribute("titulo", "Resultado form");
+			/*
+			 * Map<String, String> errores = new HashMap<>();
+			 * result.getFieldErrors().forEach(err ->{ errores.put(err.getField(),
+			 * "El campo ".concat(err.getField()).concat(" ").concat(err.getDefaultMessage()
+			 * )); }); model.addAttribute("error", errores);
+			 */
 			return "form";
 		}
+
+		return "redirect:/ver";
+	}
+
+	@GetMapping("/ver")
+	public String ver(@SessionAttribute(name="usuario", required=false) Usuario usuario, Model model, SessionStatus status) {
 		
-		model.addAttribute("usuario", usuario);
+		if(usuario==null) {
+			return "redirect:/form";
+		}
+		model.addAttribute("titulo", "Resultado form");
 		status.setComplete();
-		
 		return "resultado";
 	}
 }
