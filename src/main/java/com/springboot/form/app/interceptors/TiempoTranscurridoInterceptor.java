9bc515a02e92929d677ea74f9d2600e18943f3cd
cesardavid26 <cesardavidcp@ufps.edu.co>
2021-02-02 21:47:53 -0500

    registrando interceptor en la config de Spring

--- src/main/java/com/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
+++ src/main/java/com/springboot/form/app/interceptors/TiempoTranscurridoInterceptor.java
@@ -7,12 +7,13 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.Logger;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 
 
-@Component
+@Component("tiempoTranscurridoInterceptor")
 public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
 
 	private static final Logger logger = org.slf4j.LoggerFactory.getLogger(TiempoTranscurridoInterceptor.class); 
@@ -20,7 +21,14 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		
+		 if(handler instanceof HandlerMethod ) {
+			 HandlerMethod metodo = (HandlerMethod) handler;
+			 logger.info("es un metodo del controlador: " + metodo.getMethod().getName());
+		 }
+		
 		logger.info("TiempoTranscurridoInterceptor: preHandle() entrando...");
+		logger.info("Interceptando: " +handler);
 		long tiempoInicio = System.currentTimeMillis();
 		request.setAttribute("tiempoInicio", tiempoInicio);
 		
@@ -39,7 +47,7 @@ public class TiempoTranscurridoInterceptor implements HandlerInterceptor{
 		long tiempoInicio = (long) request.getAttribute("tiempoInicio");
 		long tiempoTranscurrido = tiempoFin - tiempoInicio;
 		
-		if(modelAndView!=null) {
+	     if(handler instanceof HandlerMethod && modelAndView!=null) {
 			modelAndView.addObject("tiempoTranscurrido", tiempoTranscurrido);
 		}
 		
